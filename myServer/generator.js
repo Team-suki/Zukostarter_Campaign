0552120805b40aa346e9b61be68927365e308059
Tokki0401 <gyuhun95@gmail.com>
2020-07-10 17:08:00 -0700

    csv file generator refactored

--- myServer/generator.js
+++ myServer/generator.js
@@ -4,71 +4,101 @@ const random = require('random');
 const fs = require('fs');
 
 // Faker + Media Asset Generator
-function generateFakeData() {
+function generateFakeData(itemID) {
   return {
+    id: itemID,
+
     gif1: `https://sdc-media-data.s3-us-west-1.amazonaws.com/giphys/giphy+(${random.int(min=1, max=10)}).webp`,
     gif2: `https://sdc-media-data.s3-us-west-1.amazonaws.com/giphys/giphy+(${random.int(min=1, max=10)}).webp`,
     gif3: `https://sdc-media-data.s3-us-west-1.amazonaws.com/giphys/giphy+(${random.int(min=1, max=10)}).webp`,
     image1: `https://sdc-media-data.s3-us-west-1.amazonaws.com/Images/download+(${random.int(min=1, max=20)}).jpg`,
     image2: `https://sdc-media-data.s3-us-west-1.amazonaws.com/Images/download+(${random.int(min=1, max=20)}).jpg`,
     image3: `https://sdc-media-data.s3-us-west-1.amazonaws.com/Images/download+(${random.int(min=1, max=20)}).jpg`,
-    storyTitle1: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    storyTitle2: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    storyTitle3: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    storyTitle4: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    storyTitle5: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    storyText1: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    storyText2: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    storyText3: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    storyText4: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    storyText5: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
+    storyT1: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    storyT2: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    storyT3: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    storyT4: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    storyT5: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    storyText1: faker.lorem.paragraph(),
+    storyText2: faker.lorem.paragraph(),
+    storyText3: faker.lorem.paragraph(),
+    storyText4: faker.lorem.paragraph(),
+    storyText5: faker.lorem.paragraph(),
 
-    riskTitle1: faker.hacker.noun(),
-    riskTitle2: faker.hacker.noun(),
-    riskTitle3: faker.hacker.noun(),
-    riskTitle4: faker.hacker.noun(),
-    riskTitle5: faker.hacker.noun(),
-    riskText1: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    riskText2: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    riskText3: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    riskText4: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    riskText5: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
+    riskT1: faker.hacker.noun(),
+    riskT2: faker.hacker.noun(),
+    riskT3: faker.hacker.noun(),
+    riskT4: faker.hacker.noun(),
+    riskT5: faker.hacker.noun(),
+    riskText1: faker.lorem.paragraph(),
+    riskText2: faker.lorem.paragraph(),
+    riskText3: faker.lorem.paragraph(),
+    riskText4: faker.lorem.paragraph(),
+    riskText5: faker.lorem.paragraph(),
 
-    commitTitle1: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    commitTitle2: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    commitTitle3: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    commitTitle4: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    commitTitle5: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
-    commitText1: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    commitText2: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    commitText3: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    commitText4: faker.fake('{{hacker.phrase}} {{hacker.phrase}}'),
-    commitText5: faker.fake('{{hacker.phrase}} {{hacker.phrase}}')
+    commitT1: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    commitT2: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    commitT3: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    commitT4: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    commitT5: faker.fake('{{hacker.ingverb}} {{hacker.noun}}'),
+    commitText1: faker.lorem.paragraph(),
+    commitText2: faker.lorem.paragraph(),
+    commitText3: faker.lorem.paragraph(),
+    commitText4: faker.lorem.paragraph(),
+    commitText5: faker.lorem.paragraph(),
   }
 }
 
+function csvFormat(data) {
+  return headers.reduce((acc, header, index) => {
+    if (item === 0) {
+      if (index === headers.length - 1) {
+        acc += header;
+      } else {
+        acc += header + ',';
+      }
+    } else {
+      if (index === headers.length - 1) {
+        acc += data[header];
+      } else {
+        acc += data[header] + ',';
+      }
+    }
+
+    return acc;
+  }, '');
+
+}
 
 // WriteStream
-let writeStream = fs.createWriteStream('../example.csv')
+let writeStream = fs.createWriteStream('../trial.csv')
+  .on('finsh', () => console.log('Finished'))
   .on('error', (err) => { console.log('error occured:', err)});
 
 // Start Timer
 let startTime = Date.now();
 
-var itemsLeft = 3; // 10,000,000
+var item = 0;
+var itemsLeft = 2; // 10000000
+var headers = [
+  'id', 'gif1', 'gif2', 'gif3', 'image1', 'image2', 'image3',
+  'storyT1', 'storyT2', 'storyT3', 'storyT4', 'storyT5', 'storyText1', 'storyText2', 'storyText3', 'storyText4', 'storyText5',
+  'riskT1', 'riskT2', 'riskT3', 'riskT4', 'riskT5', 'riskText1', 'riskText2', 'riskText3', 'riskText4', 'riskText5',
+  'commitT1', 'commitT2', 'commitT3', 'commitT4', 'commitT5', 'commitText1', 'commitText2', 'commitText3', 'commitText4', 'commitText5'
+];
 
 function writeToCSV() {
   let keepWriting = true;
 
   // while item is less than itemsLeft and writeStream.write(data, 'utf8')
-  while (keepWriting && 0 < itemsLeft) {
-    keepWriting = writeStream.write(JSON.stringify(generateFakeData()) + '\n', 'utf8');
-    itemsLeft--;
+  while (keepWriting && item <= itemsLeft) {
+    keepWriting = writeStream.write(csvFormat(generateFakeData(item)) + '\n', 'utf8');
+    item++;
   }
 
   // exit while loop when keepWriting = false;
   // if itemsLeft > 0
-  if (itemsLeft > 0) {
+  if (itemsLeft > item) {
     // drain so that it can keep writing
     writeStream.once('drain', writeToCSV);
   } else {
